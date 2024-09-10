86172145e5d27e93e975c4855cc730598e0bb939
joseSincal <ottozincal@gmail.com>
2024-09-10 13:30:39 -0600

    add hello word

---
+++ src/main/java/com/platzi/course/PlatziCourseApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/course/PlatziCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

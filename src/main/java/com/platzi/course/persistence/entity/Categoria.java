5ad790afd3c9f6c6b614332f2bd948df51d87410
joseSincal <ottozincal@gmail.com>
2024-09-16 20:10:24 -0600

    add entities

---
+++ src/main/java/com/platzi/course/persistence/entity/Categoria.java
@@ -0,0 +1,17 @@
+package com.platzi.course.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+}
